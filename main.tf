63c153b7b0e89910c39550e3aa9f18abf31658cd
Vladyslav Ovchynnykov <10961029+pythad@users.noreply.github.com>
2022-10-13 19:35:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
-module "sg" {
-	source  = "f7d0748b41b1.test-env.scalr.com/env-svrcnchebt61e30/aws/sg"
-	version = "1.0.1"
+provider "aws" {
+  region = var.region
+}
+
+module "instance" {
+  source  = "f7d0748b41b1.test-env.scalr.com/env-svrcnchebt61e30/instance/aws"
+  version = "1.0.1"
+  instance_type = var.instance_type
+  instance_count = var.instance_count
+  subnet = var.subnet
+  sg = var.security_group
+  key = var.ssh_key
+  vpc_id = var.vpc_id
+  ami = var.ami
 }
