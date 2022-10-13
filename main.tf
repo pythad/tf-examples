9e154f3409365f95de7d57201ae58a945e7146ff
Vladyslav Ovchynnykov <10961029+pythad@users.noreply.github.com>
2022-10-13 19:38:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,4 @@ provider "aws" {
 module "instance" {
   source  = "f7d0748b41b1.test-env.scalr.com/env-svrcnchebt61e30/instance/aws"
   version = "1.0.1"
-  instance_type = var.instance_type
-  instance_count = var.instance_count
-  subnet = var.subnet
-  sg = var.security_group
-  key = var.ssh_key
-  vpc_id = var.vpc_id
-  ami = var.ami
 }
